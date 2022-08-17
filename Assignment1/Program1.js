edf32648a8e7f994f2dd5b26946478d699899edb
BhavyaGupta21 <72260665+BhavyaGupta21@users.noreply.github.com>
2022-08-17 21:23:14 +0530

    first commit

---
+++ Assignment1/Program1.js
@@ -0,0 +1,8 @@
+// Program 1- Write a program to add 2 numbers and print the result on the console.
+
+let num1 = 100;
+let num2 = 200;
+
+let sum = num1 + num2
+
+console.log("Sum of the two numbers is: " + sum);
