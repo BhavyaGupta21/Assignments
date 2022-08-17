9f4139b8ecd6bd7cbcc14892c2c4956b53da5ad9
BhavyaGupta21 <72260665+BhavyaGupta21@users.noreply.github.com>
2022-08-17 21:40:34 +0530

    Added Program 3

---
+++ Assignment1/Program3.js
@@ -0,0 +1,9 @@
+// Program 3- Write a program to display student results. If the number is below 50 then print fail and if the number is above 50 then pass.
+
+let result = 60;
+
+if(result > 50) {
+    console.log("Pass");
+} else {
+    console.log("Fail");
+}
