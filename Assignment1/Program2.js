9e3e5058684e362710152f4e701dc06ed9405590
BhavyaGupta21 <72260665+BhavyaGupta21@users.noreply.github.com>
2022-08-17 21:30:58 +0530

    Added Program 2

---
+++ Assignment1/Program2.js
@@ -0,0 +1,8 @@
+// Program 2- Write a program to subtract 2 numbers and print the result on the console
+
+let num1 = 1000;
+let num2 = 500;
+
+let result = num1 - num2;
+
+console.log("Result after subtracting the two numbers is: " + result);
