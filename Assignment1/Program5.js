7e0d609ea7c14bcc444e2a11f0f8bdcd727d7de3
BhavyaGupta21 <72260665+BhavyaGupta21@users.noreply.github.com>
2022-08-17 22:05:05 +0530

    Changes made in Progarm 5

--- Assignment1/Program5.js
+++ Assignment1/Program5.js
@@ -1,3 +1,5 @@
+// Program 5- Write a function that takes 3 numbers as arguments and perform multiplication of the numbers and returns the output. 
+
 let multiply = ( FirstNumber, secondNumber, thirdNumber) => { return FirstNumber*secondNumber*thirdNumber }
 
 console.log(multiply(12,23,24));
