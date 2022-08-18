8396e33187dd8894cf78bcfb83748311f7df7a17
BhavyaGupta21 <72260665+BhavyaGupta21@users.noreply.github.com>
2022-08-18 09:52:33 +0530

    Added first program of Assignment 2

---
+++ Assignment2/Program1.js
@@ -0,0 +1,13 @@
+// Program 1- Write a program to calculate the average of 5 numbers.
+
+let num1 = 100;
+let num2 = 200;
+let num3 = 300;
+let num4 = 400;
+let num5 = 500;
+
+let sum = num1 + num2 + num3 + num4 + num5;
+
+let average = sum / 5;
+
+console.log("Average of five numbers is: " + average);
