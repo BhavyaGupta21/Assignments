0456fc074a319800bf29aea9ab1d9c32824e45b1
BhavyaGupta21 <72260665+BhavyaGupta21@users.noreply.github.com>
2022-08-18 09:55:50 +0530

    Added Program 2

---
+++ Assignment2/Program2.js
@@ -0,0 +1,11 @@
+// Program 2- Write a program to calculate the sum of 5 numbers.
+
+let num1 = 100;
+let num2 = 200;
+let num3 = 300;
+let num4 = 400;
+let num5 = 500;
+
+let sum = num1 + num2 + num3 + num4 + num5;
+
+console.log("Sum of five numbers is: " + sum);
